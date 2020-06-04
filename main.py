6b91d94aa7930c68f9dd039c747be6a03f575b94
bruhascended <arlakgarihc@gmail.com>
2020-06-04 13:51:08 +0530

    fixed

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ same = list(csv.reader(open(root + 'same.csv', encoding='utf8')))
 distinct = []
 
 for twins in same:
-    distinct.append(twins[0])
+    distinct.append(int(twins[0]))
 
 bot_state = False
 
@@ -21,11 +21,11 @@ async def display_message(message):
     global bot_state
     index = int(open('pos', 'r').read())
     if index < len(distinct):
-        to_send = 'sms number #{0[0]}\nSender: {0[1]};\n{0[3]}'.format(data[distinct[index]])
+        to_send = 'sms number #{1}\nSender: {0[1]};\n{0[2]}'.format(data[distinct[index] + 1], index)
     else:
         bot_state = False
-        to_send = 'All of data set is labelled!'\
-                  '------------END------------\n'
+        to_send = 'All of data set is labelled!\n'\
+                  '------------END------------'
     await message.channel.send(to_send)
 
 
@@ -49,13 +49,13 @@ class MyClient(discord.Client):
         elif _content.find('sms start') > -1:
             bot_state = True
             await message.channel.send('1 for personal\n2 for transaction\n3 for advertisement\n'
-                                       '4 for spam/fraud\n5 for not clear'
-                                       '------------------START----------------\n')
+                                       '4 for spam/fraud\n5 for not clear\n'
+                                       '------------------START----------------')
             await display_message(message)
 
         elif _content.isdigit() and int(_content) in range(1, 6) and bot_state:
             index = int(open('pos', 'r').read())
-            open('pos', 'w').write(str(index))
+            open('pos', 'w').write(str(index + 1))
             open("labels.csv", "a").write('\n{0},{1}'.format(distinct[index], message.clean_content))
             await display_message(message)
 
